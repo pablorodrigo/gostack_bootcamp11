ab7d6da935770176b3ec9ffd34f97befb66ca42c
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-20 23:28:51 -0400

    [NodeJS][Intro][Learning][Backend] - Types of params

--- codes/node_intro/src/index.js
+++ codes/node_intro/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // get information from API
 app.get('/projects', (request, response) => {
     return response.json(['Project1', 'Project2']
@@ -9,11 +11,17 @@ app.get('/projects', (request, response) => {
 });
 // create new information
 app.post('/projects', (request, response) => {
+
+    console.log(request.body)
+
     return response.json(['Project1', 'Project2']
     )
 });
 // update an information
 app.put('/projects/:id', (request, response) => {
+
+    console.log(request.params)
+
     return response.json(['Project1', 'Project2']
     )
 });

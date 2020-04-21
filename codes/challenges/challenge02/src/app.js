ef0adbde16bc4e9d785b2d18fbc8a13b73eee73c
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-21 01:44:54 -0400

    [NodeJS][Challenge02] - Created CRUD

--- codes/challenges/challenge02/src/app.js
+++ codes/challenges/challenge02/src/app.js
@@ -17,16 +17,48 @@ app.get("/repositories", (request, response) => {
 
 // create new repository
 app.post("/repositories", (request, response) => {
-    // TODO
+    const {title, url, techs} = request.body
+
+    const repository = {id: uuid(), title, url, techs, likes: 0};
+
+    repositories.push(repository);
+
+    return response.json(repositories);
 });
 
 // update a repository
 app.put("/repositories/:id", (request, response) => {
-    // TODO
+    const {id} = request.params;
+    const {title, url, techs} = request.body
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({error: 'Repository not found'})
+    }
+
+    repositories[repositoryIndex] = {
+        id,
+        title,
+        url,
+        techs
+    };
+
+    return response.json(repositories)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-    // TODO
+    const {id} = request.params;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({error: 'Repository not found'})
+    }
+
+    repositories.splice(repositoryIndex, 1)
+
+    return response.status(200).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {

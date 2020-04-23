43b9b6ce25ee986ce43d3fa216d5072dff75f122
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-23 16:54:26 -0400

    [NodeJS][Learning][FirstCompleteApp] - ESLint, TS, PRettier configuration

--- codes/gobarber/backend/src/server.ts
+++ codes/gobarber/backend/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request,response)=>{
-    return response.json({message: 'hello teste'})
-})
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('🚀 server running! 🚀')
+  console.log('🚀 server running! 🚀');
 });

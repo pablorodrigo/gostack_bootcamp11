79eb873e85db1789f2925a26cf3762eded0717d5
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-22 22:40:27 -0400

    [ReactNative][Learning][Intro] - List of projects from API

--- codes/reactnative_intro/src/App.js
+++ codes/reactnative_intro/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+} from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle={'light-content'} backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello React Native</Text>
-      </View>
+      {/*<View style={styles.container}>
+        {projects.map((project) => (
+          <Text key={project.id} style={styles.title}>
+            {project.title}
+          </Text>
+        ))}
+      </View>*/}
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item: project}) => (
+            <Text key={project.id} style={styles.title}>
+              {project.title}
+            </Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -16,12 +50,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   title: {
     color: '#FFF',
-    fontSize: 32,
+    fontSize: 30,
     fontWeight: 'bold',
   },
 });

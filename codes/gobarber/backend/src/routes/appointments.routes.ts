4289bbb5c71264044940d19460974955a005ba63
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-23 21:46:23 -0400

    [NodeJS][Learning][FirstCompleteApp][gobarber][backend] - Create appointment

---
+++ codes/gobarber/backend/src/routes/appointments.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+
+
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  console.log(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;

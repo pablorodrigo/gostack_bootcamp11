1d952456b3673d126f54bc9b05765f18b2ad6df1
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-26 17:06:09 -0400

    [NodeJS][Learning][FirstCompleteApp][gobarber][backend] - Configurede entity Appointment to send to DB

--- codes/gobarber/backend/src/models/Appointment.ts
+++ codes/gobarber/backend/src/models/Appointment.ts
@@ -1,10 +1,18 @@
-import { uuid } from 'uuidv4';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
 
   /* constructor(provider: string, date: Date) {
@@ -13,11 +21,11 @@ class Appointment {
     this.date = date;
   } */
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  /*constructor({ provider, date }: Omit<Appointment, 'id'>) {
     this.id = uuid();
     this.provider = provider;
     this.date = date;
-  }
+  }*/
 }
 
 export default Appointment;

b8170eccdd87a1eae28e4312c270a5c4aea5ceae
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-02 23:44:33 -0400

    [ReactJS][Learning][FirstCompleteApp][frontend] - Configured API connection

--- codes/first_react_project_typescript/src/routes/pages/Dashboard/index.tsx
+++ codes/first_react_project_typescript/src/routes/pages/Dashboard/index.tsx
@@ -1,34 +1,61 @@
-import React from 'react';
-
-import {FiChevronRight} from 'react-icons/fi';
-import {Form, Title, Repositories} from './styles';
+import React, { FormEvent, useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../../services/api';
+import { Form, Title, Repositories } from './styles';
 
 import logoImg from '../../../assets/logo.svg';
 
+// I should put when I know types of data returned from API
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepository, setNewRepository] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  // it will add new repository
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`/repos/${newRepository}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+    setNewRepository('');
+  }
+
   return (
     <>
-      <img src={logoImg}
-           alt="Github Explorer" />
+      <img src={logoImg} alt="Github Explorer" />
       <Title>GitHub Repositories</Title>
-      <Form>
-        <input placeholder="Type the name of repository" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepository}
+          onChange={(text) => setNewRepository(text.target.value)}
+          placeholder="Type the name of repository"
+        />
         <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars0.githubusercontent.com/u/6105149?s=460&u=d58cec4a12fcb354be57b50e72180f9de07a8393&v=4"
-            alt="Pablo Perfil"
-          />
-          <div>
-            <strong>rocketseat</strong>
-            <p>text text text</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="test">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );

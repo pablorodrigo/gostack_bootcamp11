571d7ecf218b33557ddb59512c5f0539d14cf39c
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-04 18:25:18 -0400

    [ReactJS][Learning][gobarber] - Login page created

--- codes/gobarber/web/src/App.tsx
+++ codes/gobarber/web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index';
 
 const App: React.FC = () => (
   <>
-    <p>Hello react</p>
+    <SignIn />
     <GlobalStyle />
   </>
 );

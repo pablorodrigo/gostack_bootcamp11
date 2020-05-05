cd69ac1858c55c5e0d15ea3e25bfaf9bcfd3db8a
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-05 12:15:25 -0400

    [ReactJS][Learning][gobarber] - Created validation using Yup and showing the error if occur

--- codes/gobarber/web/src/pages/SignUp/index.tsx
+++ codes/gobarber/web/src/pages/SignUp/index.tsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/all';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
-
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
+  const formRef = useRef<FormHandles>(null);
+
+  console.log(formRef.current);
+
+  const handleSubmit = useCallback(async (data: object) => {
     console.log(data);
-  }
+
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatorio'),
+        email: Yup.string().required('Email obrigatorio').email('Digite um email valido'),
+        password: Yup.string().min(6, 'No minimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+
+      const errors = getValidationErrors(error);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="Gobarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1> Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />

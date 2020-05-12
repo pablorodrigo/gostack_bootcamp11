b767205ca01d8f9aa16386ce7bb2e132d56eeffc
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-12 14:56:01 -0400

    [ReactNative][Learning][gobarber][app] - Cofigured how to use external fonts, Input and Button components

--- codes/gobarber/app/src/pages/SignIn/index.tsx
+++ codes/gobarber/app/src/pages/SignIn/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
-import { Container } from './styles';
+import { Image } from 'react-native';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail"/>
+      <Input name="password" icon="lock" placeholder="Senha"/>
+
+      <Button>Entrar</Button>
+    </Container>
+  );
 };
 
 export default SignIn;

ef14424c58c6c50d180d00493f389ad799c35ce0
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-13 10:00:04 -0400

    [ReactNative][Learning][gobarber][app] - Created NewUser page and navigation between pages

--- codes/gobarber/app/src/pages/SignUp/index.tsx
+++ codes/gobarber/app/src/pages/SignUp/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
-
-import { Container } from './styles';
+import { View, Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Title, BackToSignInButton, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button>Criar</Button>
+          </Container>
+        </ScrollView>
+        <BackToSignInButton onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignInButton>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
 export default SignUp;

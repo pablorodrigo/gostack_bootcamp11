c675398a73a73f495aafc6320566d111a8ccd33b
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-12 22:44:16 -0400

    [ReactNative][Learning][gobarber][app] - Created Authentication page

--- codes/gobarber/app/src/pages/SignIn/index.tsx
+++ codes/gobarber/app/src/pages/SignIn/index.tsx
@@ -1,21 +1,50 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { View, Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail"/>
-      <Input name="password" icon="lock" placeholder="Senha"/>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button>Entrar</Button>
-    </Container>
+            <Button>Entrar</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+        <CreateAccountButton>
+          <Icon name="log-in" size={20} color="#ff9000" />
+
+          <CreateAccountButtonText>Faca sua contyar</CreateAccountButtonText>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 

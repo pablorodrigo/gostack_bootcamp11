b4d0487b6fba5032ab6cd68b1184087193aea915
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-25 15:22:02 -0400

    [NodeJS][Learning][gobarber][backend] - Completed Authentication tests

--- codes/gobarber/backend/src/tests/modules/users/services/AuthenticateUserService.spec.ts
+++ codes/gobarber/backend/src/tests/modules/users/services/AuthenticateUserService.spec.ts
@@ -34,4 +34,49 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'judidoe@email.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password/email', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'Judi Doe',
+      email: 'judidoe@email.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'judidoe@email.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });

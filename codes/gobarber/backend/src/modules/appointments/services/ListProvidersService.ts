272d6846001613cbe6647350364030b100e53aac
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-06-11 09:51:42 -0400

    [NodeJS][Learning][gobarber][backend] - Congifured Cache for list of providers

--- codes/gobarber/backend/src/modules/appointments/services/ListProvidersService.ts
+++ codes/gobarber/backend/src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import { inject, injectable } from 'tsyringe';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -12,17 +13,30 @@ export default class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    const cashUsers = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
-    if (users.length === 0) {
-      throw new AppError('Empty list');
+    if (!cashUsers) {
+      const users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      if (users.length === 0) {
+        throw new AppError('Empty list');
+      }
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+
+      return users;
     }
 
-    return users;
+    return cashUsers;
   }
 }

4ca7f4b8452b1bcb95e3881f940947179f840dfe
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-06-26 22:27:26 -0400

    [NodeJS][Learning][gobarber][backend] - Updated server to use rateLimit after files request

--- codes/gobarber/backend/src/shared/infra/http/server.ts
+++ codes/gobarber/backend/src/shared/infra/http/server.ts
@@ -13,10 +13,10 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLimit);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimit);
 app.use(routes);
 app.use(errors());
 app.use(

f1565360a9d59513f1c1f0108ff8e2dd681b3206
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-07-07 23:16:18 -0400

    [ReactNative][Learning][gobarber][app] - Created image adress and the routes, pages that will be used

---
+++ codes/gobarber/app/src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+const AppointmentCreate: React.FC = () => {
+  return (
+    <View>AppointmentCreate</View>
+  );
+};
+
+export default AppointmentCreate;
---
+++ codes/gobarber/app/src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+const CreateAppointment: React.FC = () => {
+  return (
+    <View>CreateAppointment</View>
+  );
+};
+
+export default CreateAppointment;

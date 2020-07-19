e7c5ae917c3c7cc58742f52ea555c6f3d7065f3c
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-07-19 10:07:41 -0400

    [ReactNative][Learning][gobarber][app] - Create CreateAppointment screen structure

--- codes/gobarber/app/src/pages/CreateAppointment/index.tsx
+++ codes/gobarber/app/src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { BackButton, Container, Header, HeaderTitle, UserAvatar } from './styles';
+import { useAuth } from '../../hooks/auth';
 
-import {Text, View} from 'react-native';
+interface IRouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+  const { providerId } = route.params as IRouteParams;
+
+  console.log(route.params);
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
-    <Text>CreateAppointment</Text>
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={25} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
   );
 };
 
